941d22b2bfbd91f323b99521783ab917e8dad20a
Sebastian Werner <s.werner@sebastian-software.de>
2017-05-26 12:44:03 +0200

    Added flow:coverage tool integration

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,10 @@
 import "readable-code"
+
+import gulp from "gulp"
+import spawn from "cross-spawn"
+
+export const execSync = spawn.sync
+
+gulp.task("flow:coverage", () => {
+  console.log(execSync("flow-coverage-report", [ "-i", "src/**/*.js" ], { stdio: "pipe" }).stdout.toString())
+})
