f49641a988cf198289e5fa85e76c9139841b9534
Sebastian Werner <s.werner@sebastian-software.de>
2017-05-31 11:42:56 +0200

    Worked on fixing component tests

--- stories/index.js
+++ stories/index.js
@@ -2,10 +2,37 @@ import React from "react"
 import { storiesOf } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
 
+import { IntlProvider } from "react-intl"
+import { BrowserRouter } from "react-router-dom"
+
+import Header from "../src/components/Header"
+import Navigation from "../src/components/Navigation"
+
 storiesOf("Button", module)
   .add("with text", () => (
     <button onClick={action("clicked")}>Hello Button</button>
   ))
   .add("with some emoji", () => (
     <button onClick={action("clicked")}>😀 😎 👍 💯</button>
+  ))
+
+const messages = {
+  "app.title": "Storybook Title",
+  "app.description": "Storybook Description"
+}
+
+storiesOf("Header", module)
+  .add("Site Header", () => (
+    <IntlProvider locale="en" messages={messages}>
+      <Header/>
+    </IntlProvider>
+  ))
+
+storiesOf("Navigation", module)
+  .add("Site Navigation", () => (
+    <IntlProvider locale="en" messages={messages}>
+      <BrowserRouter>
+        <Navigation/>
+      </BrowserRouter>
+    </IntlProvider>
   ))
